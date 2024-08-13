d7d15e413dd71816392cc2f0d8e64d27d5defff3
andkorzh <93550076+andkorzh@users.noreply.github.com>
2024-08-13 08:44:49 +0300

    fix cout

--- Verilog/Core6502.v
+++ Verilog/Core6502.v
@@ -1015,7 +1015,7 @@ assign RESULT[7:0] = ({8{ANDS}} & ANDo[7:0]) | ({8{ORS}} & ORo [7:0]) | ({8{EORS
 wire [7:0]CIN;	
 assign CIN[7:0] = { COUT[6:4], DCOUT3, COUT[2:0], ~_ACIN };  	// assign CIN[7:0] = { COUT[6:0], ~_ACIN };	// { COUT[6:4], DCOUT3, COUT[2:0], ~_ACIN };
 wire [7:0]COUT;
-assign COUT[7:0] = ( CIN[7:0] & ORo[7:0] ) | ANDo[7:0] ;
+assign COUT[7:0] = ( CIN[7:0] & XORo[7:0] ) | ANDo[7:0];
 wire DCOUT3;
 assign DCOUT3 = COUT[3] | DC3;
 assign ACR = LATCH_C7 | LATCH_DC7;	               //	ACR = LATCH_C7 | LATCH_DC7;
